9ae7c5c69de20171c58b5da059b2fb644f7dce59
Francesco Casula <fra.casula@gmail.com>
2018-02-16 18:29:30 +0000

    meh

--- http/router/router_test.go
+++ http/router/router_test.go
@@ -23,6 +23,7 @@ func TestInitRoutes(t *testing.T) {
 
 func TestNewRouter(t *testing.T) {
 	routes := InitRoutes()
+	emptyHandler := func(w http.ResponseWriter, req *http.Request, vars map[string]string) {}
 
 	routes["^/help$"] = emptyHandler
 	routes["^/contact$"] = emptyHandler
@@ -52,7 +53,7 @@ func TestNewRouterPanic(t *testing.T) {
 	}()
 
 	routes := InitRoutes()
-	routes["a broken regex???"] = emptyHandler
+	routes["a broken regex???"] = func(w http.ResponseWriter, req *http.Request, vars map[string]string) {}
 
 	NewRouter(&routes)
 }
@@ -60,12 +61,12 @@ func TestNewRouterPanic(t *testing.T) {
 func TestRouting(t *testing.T) {
 	routes := InitRoutes()
 
-	routes["^/help$"] = func(w http.ResponseWriter, req *http.Request, vars map[string]string) {
+	routes["^/help$"] = func(w http.ResponseWriter, req *http.Request, _ map[string]string) {
 		fmt.Fprint(w, "Hello from the help page!")
 	}
 
-	routes["^/product/(?P<pid>[0-9]+)$"] = func(w http.ResponseWriter, req *http.Request, vars map[string]string) {
-		fmt.Fprintf(w, "Product ID is %v", vars["pid"])
+	routes["^/another/page$"] = func(w http.ResponseWriter, req *http.Request, _ map[string]string) {
+		fmt.Fprintf(w, "Hello from another page!")
 	}
 
 	router := NewRouter(&routes)
@@ -80,29 +81,57 @@ func TestRouting(t *testing.T) {
 		t.Errorf("Routing to help page failed, got %d: %v", w.Code, w.Body)
 	}
 
-	// Testing params
+	// Testing another page
 	w = httptest.NewRecorder()
-	req, _ = http.NewRequest("GET", "/product/135", strings.NewReader(""))
+	req, _ = http.NewRequest("GET", "/another/page", strings.NewReader(""))
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK || w.Body.String() != "Hello from another page!" {
+		t.Errorf("Routing to help page failed, got %d: %v", w.Code, w.Body)
+	}
+}
+
+func TestRoutingWithParams(t *testing.T) {
+	routes := InitRoutes()
+
+	routes["^/product$"] = func(w http.ResponseWriter, req *http.Request, _ map[string]string) {
+		fmt.Fprint(w, "Hello from the product page!")
+	}
+
+	routes["^/product/(?P<pid>[0-9]+)$"] = func(w http.ResponseWriter, req *http.Request, vars map[string]string) {
+		fmt.Fprintf(w, "Product ID is %v", vars["pid"])
+	}
+
+	router := NewRouter(&routes)
+
+	// Testing params
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/product/135", strings.NewReader(""))
 
 	router.ServeHTTP(w, req)
 
 	if w.Code != http.StatusOK || w.Body.String() != "Product ID is 135" {
 		t.Errorf("Routing to help page failed, got %d: %v", w.Code, w.Body)
 	}
+}
+
+func TestRoutingNotFound(t *testing.T) {
+	routes := InitRoutes()
+
+	routes["^/page$"] = func(w http.ResponseWriter, req *http.Request, _ map[string]string) {
+		fmt.Fprint(w, "Hello from a page!")
+	}
+
+	router := NewRouter(&routes)
 
 	// Testing not found handler
-	w = httptest.NewRecorder()
-	req, _ = http.NewRequest("GET", "/a/not/found/page", strings.NewReader(""))
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/a/not/found/page", strings.NewReader(""))
 
 	router.ServeHTTP(w, req)
 
 	if w.Code != http.StatusNotFound || w.Body.String() != "404 Not Found" {
 		t.Errorf("Routing to help page failed, got %d: %v", w.Code, w.Body)
 	}
-}
-
-// functions
-
-func emptyHandler(w http.ResponseWriter, req *http.Request, vars map[string]string) {
-	// nothing to do here
 }
